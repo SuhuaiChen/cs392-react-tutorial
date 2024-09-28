203a4d0bd68557db2885c981f3eea511ff95816e
Suhuai <suhuaichen2024@u.northwestern.edu>
2024-09-28 10:41:40 -0500

    Show a list of courses

---
+++ src/components/CourseList.jsx
@@ -0,0 +1,23 @@
+const terms = { F: 'Fall', W: 'Winter', S: 'Spring'};
+
+const getCourseTerm = course => (
+  terms[course.id.charAt(0)]
+);
+
+const getCourseNumber = course => (
+  course.id.slice(1, 4)
+);
+
+const Course = ({ course }) => (
+  <div>
+    { getCourseTerm(course) } CS { getCourseNumber(course) }: { course.title }
+  </div>
+);
+
+const CourseList = ({ courses }) => (
+    <div>
+      { Object.values(courses).map(course => <Course key={course.id} course={ course } />) }
+    </div>
+);
+
+export default CourseList
