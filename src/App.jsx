d5d2a791dd86c71c7073e196cefec862d3284b43
Suhuai <suhuaichen2024@u.northwestern.edu>
2024-09-29 18:49:17 -0500

    Enable selecting courses without time conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,40 @@ import Banner from './components/Banner';
 import CourseList from './components/CourseList';
 import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query'
 
+const meetsPat = /^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/;
+
+const timeParts = meets => {
+  const [match, days, hh1, mm1, hh2, mm2] = meetsPat.exec(meets) || [];
+  return !match ? {} : {
+    days,
+    hours: {
+      start: hh1 * 60 + mm1 * 1,
+      end: hh2 * 60 + mm2 * 1
+    }
+  };
+};
+
+const mapValues = (fn, obj) => (
+  Object.fromEntries(Object.entries(obj).map(([key, value]) => [key, fn(value)]))
+);
+
+const addCourseTimes = course => ({
+  ...course,
+  ...timeParts(course.meets)
+});
+
+const addScheduleTimes = schedule => ({
+  title: schedule.title,
+  courses: mapValues(addCourseTimes, schedule.courses)
+});
+
 const fetchSchedule = async () => {
   const url = 'https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php';
   const response = await fetch(url);
   try {
     const res = await response.json();
     console.log(res);
-    return res;
+    return addScheduleTimes(res);
   }
   catch (error) {
     console.log(error);
